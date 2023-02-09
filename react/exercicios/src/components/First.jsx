25e51e3f1f46ec1e11b1f56ce92205d9d085857f
Jairo <jairohneto@gmail.com>
2023-02-09 19:42:33 -0300

    first commit

---
+++ react/exercicios/src/components/First.jsx
@@ -0,0 +1,2 @@
+export  default () => <h1>First</h1>
+  
---
+++ react/exercicios/src/components/GoodMorning.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export  default props => <h1>Bom dia, meu nome é {props.nome}, eu tenho {props.idade} anos de idade!</h1>
